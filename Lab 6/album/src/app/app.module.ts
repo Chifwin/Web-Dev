f2363234db9b0ae2f644d1fc8f59a7b566e74732
Chifwin <o.s.merezhko@gmail.com>
2023-03-06 17:16:10 +0600

    try

--- Lab 6/album/src/app/app.module.ts
+++ Lab 6/album/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { AlbumDetailComponent } from './components/album-detail/album-detail.component';
+import { AlbumPhotosComponent } from './components/album-photos/album-photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AlbumsComponent,
+    AlbumDetailComponent,
+    AlbumPhotosComponent
   ],
   imports: [
     BrowserModule
