3aba83eeacea6154a44eb81dacbea21ffa7c23d1
Chifwin <o.s.merezhko@gmail.com>
2023-04-02 14:02:34 +0600

    add

---
+++ Lab 8/django/demo/demo/urls.py
@@ -0,0 +1,36 @@
+"""demo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from django.http.response import HttpResponse
+from datetime import datetime, timedelta
+
+
+def home(request):
+    cur_t = datetime.now()
+    return HttpResponse(cur_t)
+
+
+def plus(request, data):
+    cur_t = datetime.now() + timedelta(days=int(data))
+    return HttpResponse(cur_t)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("home/", home),
+    re_path(r'^plus/(\d+)/$', plus)
+]
